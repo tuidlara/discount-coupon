708d7166a75934cc61018daf7ebf432c8a11dae7
Arthur de Lara <arthur.zilli@hotmail.com>
2026-08-25 10:35:25 -0300

    adiciona controller

---
+++ src/main/java/com/arthur/coupon_api/controller/CouponController.java
@@ -0,0 +1,26 @@
+package com.arthur.coupon_api.controller;
+
+import com.arthur.coupon_api.dto.CouponRequest;
+import com.arthur.coupon_api.dto.CouponResponse;
+import com.arthur.coupon_api.service.CouponService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/coupons")
+public class CouponController {
+
+    private final CouponService couponService;
+
+    public CouponController(CouponService couponService) {
+        this.couponService = couponService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CouponResponse createCoupon(@RequestBody CouponRequest request) {
+        return couponService.criarCupom(request);
+    }
+
+
+}
