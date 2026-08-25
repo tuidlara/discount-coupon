9b10b5a5f240b4cee32b505335909fd52f9be779
Arthur de Lara <arthur.zilli@hotmail.com>
2026-08-25 10:07:47 -0300

    subindo projeto cupom desconto

---
+++ src/main/java/com/arthur/coupon_api/entity/Coupon.java
@@ -0,0 +1,45 @@
+package com.arthur.coupon_api.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String code;
+    private Double discount;
+
+    public Coupon() {
+    }
+
+    public Coupon(String code, Double discount) {
+        this.code = code;
+        this.discount = discount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
+}
---
+++ src/test/java/com/arthur/coupon_api/CouponApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthur.coupon_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
