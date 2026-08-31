dcb0f59ccb0e7cdc11a00fb4a8ed571783889f43
Arthur de Lara <arthur.zilli@hotmail.com>
2026-08-31 09:35:57 -0300

    feat: implementa cadastro de usuário com BCrypt e role CLIENT

--- src/main/java/com/arthur/coupon_api/service/AuthService.java
+++ src/main/java/com/arthur/coupon_api/service/AuthService.java
@@ -1,22 +1,35 @@
 package com.arthur.coupon_api.service;
 
 import com.arthur.coupon_api.dto.CadastroRequest;
+import com.arthur.coupon_api.entity.Role;
+import com.arthur.coupon_api.entity.User;
 import com.arthur.coupon_api.exception.UserAlreadyExistsException;
 import com.arthur.coupon_api.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
 
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
-    public AuthService(UserRepository userRepository) {
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void cadastrar(CadastroRequest request) {
-        if(userRepository.findByEmail(request.email()).isPresent()) {
+        if (userRepository.findByEmail(request.email()).isPresent()) {
             throw new UserAlreadyExistsException("Email já cadastrado.");
         }
+        String senhaCriptografada = passwordEncoder.encode(request.senha());
+
+        User user = new User(request.email(),
+                senhaCriptografada);
+
+        user.setRole(Role.CLIENT);
+
+        userRepository.save(user);
     }
 }
